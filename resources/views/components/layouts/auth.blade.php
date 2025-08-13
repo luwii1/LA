fddda626d0ebff6339e558b75ef1bf7039a6cdcd
Layan <lyan41034@gmail.com>
2025-08-13 23:04:13 +0300

    first commit

--- resources/views/components/layouts/auth.blade.php
+++ resources/views/components/layouts/auth.blade.php
@@ -2,7 +2,7 @@
     <div class="my-auto card px-8 py-8 max-w-84 self-center">
         <a class="link text-base-content link-neutral text-3xl font-bold no-underline flex items-end justify-center text-center mb-16"
             href="/">
-            <span>عصفور</span>
+            <span>LA</span>
             <span class="icon-[tabler--brand-twitter] size-12"></span>
         </a>
         <div>
